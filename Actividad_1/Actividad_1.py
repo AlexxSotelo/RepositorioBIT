fbedf849ade695e63df5d856c5f1e67dd2cba6be
AlexxSotelo <alexxsotelop@gmail.com>
2024-07-28 21:23:15 -0500

    Se cambió el nombre de la carpeta

--- Actividad_1/Actividad_1.py
+++ Actividad_1/Actividad_1.py
@@ -11,6 +11,6 @@ tabla_2 = pd.read_csv(ruta + '\Resultados_de_An_lisis_de_Laboratorio_Suelos_en_C
 #print(tabla_2)
 
 #Se va a cargar cargar el csv de usuarios de Masterclass de Arte y Oficios
-ruta = r'C:\Users\USER\OneDrive\Escritorio\ActividadesBIT\Datasets'
+ruta = r'C:\Users\USER\OneDrive\Escritorio\ActividadesBIT\Actividad 1\Datasets'
 tabla_3 = pd.read_csv(ruta + '\Caracterizaci_n_usuarios_Master_Class_Escuela_Municipal_de_Artes_y_Oficios_-_EMA_20240728.csv', sep=',', encoding = 'utf-8')
-#print(tabla_3)
+print(tabla_3)

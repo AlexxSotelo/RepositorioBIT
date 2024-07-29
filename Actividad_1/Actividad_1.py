ae8dda7bcfb8d86a2dde1716ab1f702c62cbc5c2
AlexxSotelo <alexxsotelop@gmail.com>
2024-07-29 17:46:18 -0500

    Se añadió el formato de la primera entrega.

--- Actividad_1/Actividad_1.py
+++ Actividad_1/Actividad_1.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 #Se va a cargar el CSV de salud mental
-ruta = r'C:\Users\USER\OneDrive\Escritorio\ActividadesBIT\Datasets'
+ruta = r'C:\Users\USER\OneDrive\Escritorio\ActividadesBIT\Actividad_1\Datasets'
 tabla_1 = pd.read_csv(ruta + '\Salud_Mental_20240728.csv', sep=',', encoding = 'utf-8')
 #print(tabla_1)
 
 #Se van a cargar resultados de Análisis de suelos
-ruta= ruta = r'C:\Users\USER\OneDrive\Escritorio\ActividadesBIT\Datasets'
+ruta = r'C:\Users\USER\OneDrive\Escritorio\ActividadesBIT\Actividad_1\Datasets'
 tabla_2 = pd.read_csv(ruta + '\Resultados_de_An_lisis_de_Laboratorio_Suelos_en_Colombia_20240728.csv', sep=',', encoding = 'utf-8')
 #print(tabla_2)
 
 #Se va a cargar cargar el csv de usuarios de Masterclass de Arte y Oficios
-ruta = r'C:\Users\USER\OneDrive\Escritorio\ActividadesBIT\Actividad 1\Datasets'
+ruta = r'C:\Users\USER\OneDrive\Escritorio\ActividadesBIT\Actividad_1\Datasets'
 tabla_3 = pd.read_csv(ruta + '\Caracterizaci_n_usuarios_Master_Class_Escuela_Municipal_de_Artes_y_Oficios_-_EMA_20240728.csv', sep=',', encoding = 'utf-8')
-print(tabla_3)
+#print(tabla_3)
